2a618092ab04bab53f89c54a99184b459c08bd65
Dave Brosius <dbrosius@mebigfatguy.com>
2013-01-08 02:44:47 -0500

    add reader and writer

---
+++ src/com/mebigfatguy/yank/ArtifactReader.java
@@ -0,0 +1,70 @@
+/*
+ * yank - a maven artifact fetcher ant task
+ * Copyright 2013 MeBigFatGuy.com
+ * Copyright 2013 Dave Brosius
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.mebigfatguy.yank;
+
+import java.io.InputStream;
+import java.util.Deque;
+
+import org.apache.tools.ant.Project;
+
+class Reader implements Runnable {
+    private final Project project;
+    private final InputStream inputStream;
+    private final Deque<TransferBuffer> dq;
+    private final int bufferSize;
+    private boolean success = false;
+
+    public Reader(Project p, final InputStream is, final Deque<TransferBuffer> queue, int bufferSize) {
+        project = p;
+        inputStream = is;
+        dq = queue;
+        this.bufferSize = bufferSize;
+    }
+
+    @Override
+    public void run() {
+        try {
+            byte[] buffer = new byte[bufferSize];
+            int size = inputStream.read(buffer);
+            while (size >= 0) {
+                if (size > 0) {
+                    TransferBuffer queueBuffer = new TransferBuffer(buffer, size);
+                    synchronized (dq) {
+                        dq.addLast(queueBuffer);
+                        dq.notifyAll();
+                    }
+                    buffer = new byte[bufferSize];
+                    size = inputStream.read(buffer);
+                }
+            }
+            success = true;
+        } catch (Exception e) {
+            project.log(e.getMessage());
+        } finally {
+            TransferBuffer queueBuffer = new TransferBuffer(null, -1);
+            synchronized (dq) {
+                dq.addLast(queueBuffer);
+                dq.notifyAll();
+            }
+        }
+    }
+
+    public boolean wasSuccessful() {
+        return success;
+    }
+}
---
+++ src/com/mebigfatguy/yank/ArtifactWriter.java
@@ -0,0 +1,66 @@
+/*
+ * yank - a maven artifact fetcher ant task
+ * Copyright 2013 MeBigFatGuy.com
+ * Copyright 2013 Dave Brosius
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.mebigfatguy.yank;
+
+import java.io.OutputStream;
+import java.util.Deque;
+
+import org.apache.tools.ant.Project;
+
+class ArtifactWriter implements Runnable {
+
+    private final Project project;
+    private final OutputStream outputStream;
+    private final Deque<TransferBuffer> dq;
+    private boolean success = false;
+
+    public ArtifactWriter(Project p, final OutputStream os, final Deque<TransferBuffer> queue) {
+        project = p;
+        outputStream = os;
+        dq = queue;
+    }
+
+    @Override
+    public void run() {
+        try {
+            TransferBuffer buffer = null;
+            int size = 0;
+
+            while (size >= 0) {
+                synchronized (dq) {
+                    while (dq.size() == 0) {
+                        dq.wait();
+                    }
+                    buffer = dq.removeFirst();
+                }
+
+                size = buffer.getSize();
+                if (size > 0) {
+                    outputStream.write(buffer.getBuffer(), 0, size);
+                }
+            }
+            success = true;
+        } catch (Exception e) {
+            project.log(e.getMessage());
+        }
+    }
+
+    public boolean wasSuccessful() {
+        return success;
+    }
+}
