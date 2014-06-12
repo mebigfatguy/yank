c57e724693aeea48d6f075ea8721b44e86ee99bd
Dave Brosius <dbrosius@mebigfatguy.com>
2014-06-12 09:45:23 -0400

    oi

--- src/com/mebigfatguy/yank/LicenseGenerator.java
+++ src/com/mebigfatguy/yank/LicenseGenerator.java
@@ -23,7 +23,6 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
