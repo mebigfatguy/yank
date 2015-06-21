2110f09f6df8a492527f53252bd6bed2379ae609
Dave Brosius <dbrosius@mebigfatguy.com>
2015-06-20 23:36:20 -0400

    start getting ods parsing working

--- src/com/mebigfatguy/yank/OdsParser.java
+++ src/com/mebigfatguy/yank/OdsParser.java
@@ -22,10 +22,14 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
+import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Project;
 import org.xml.sax.Attributes;
 import org.xml.sax.InputSource;
@@ -40,25 +44,32 @@ public class OdsParser implements SpreadsheetParser {
 	private static final String TABLE_ROW = "table-row";
 	private static final String TABLE_CELL = "table-cell";
 	
+	private Project project;
+	private File odsFile;
+	private List<Artifact> artifacts = new ArrayList<Artifact>();
+	
 	@Override
-	public List<Artifact> getArtifactList(Project project, File spreadsheet) throws IOException {
+	public List<Artifact> getArtifactList(Project proj, File spreadsheet) throws IOException {
+		
+		project = proj;
+		odsFile = spreadsheet;
 		
 		ZipInputStream zipIs = null;
 		InputStream contentStream = null;
 		try {
-			zipIs = new ZipInputStream(new BufferedInputStream(new FileInputStream(spreadsheet)));
+			zipIs = new ZipInputStream(new BufferedInputStream(new FileInputStream(odsFile)));
 			contentStream = findContentStream(zipIs);
 			XMLReader r = XMLReaderFactory.createXMLReader();
 			r.setContentHandler(new OdsHandler());
 			r.parse(new InputSource(contentStream));
 		} catch (SAXException e) {
-			throw new IOException("Failed creating xml reader for ods file", e);
+			throw new IOException("Failed creating xml reader for ods file (" + odsFile +")", e);
 		} finally {
 			Closer.close(contentStream);
 			Closer.close(zipIs);
 		}
 		
-		throw new IOException("Not implemented yet");
+		return artifacts;
 	}
 
 	private InputStream findContentStream(ZipInputStream zipIs) throws IOException {
@@ -70,19 +81,117 @@ public class OdsParser implements SpreadsheetParser {
 			}
 		}
 		
-		throw new IOException("ods file has no content stream");
+		throw new IOException("ods file has no content stream (" + odsFile +")");
 	}
 	
 	class OdsHandler extends DefaultHandler {
 
+		private int curRow = 0;
+		private int curCol = 0;
+		private boolean parsingColumnHeaders = true;
+		private Map<Integer, ColumnType> columnHeaders = new HashMap<Integer, ColumnType>();
+		private StringBuilder contents = new StringBuilder();
+		private String groupId = "";
+        private String artifactId = "";
+        private String type = JAR;
+        private String version = "";
+        private String classifier = "";
+		
 		@Override
 		public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+			contents.setLength(0);
+			
 			if (TABLE_ROW.equals(localName)) {
-				
+				curRow++;
+				curCol = 0;
 			} else if (TABLE_CELL.equals(localName)) {
-				
+				contents.setLength(0);
 			}
 		}
-	}
 
+		@Override
+		public void endElement(String uri, String localName, String qName) throws SAXException {
+			if (TABLE_ROW.equals(localName)) {
+				if (parsingColumnHeaders) {
+					if (columnHeaders.size() > 0) {
+						if (columnHeaders.size() < 3)
+							throw new BuildException("Input yank ods file (" + odsFile + ") does not contains GroupId, ArtifactId, or Version columns");
+						parsingColumnHeaders = false;
+					}
+				} else {
+					if (groupId.isEmpty() || artifactId.isEmpty() || version.isEmpty()) {
+                    	if (!groupId.isEmpty() || !version.isEmpty()) {
+                    		project.log("Row " + curRow + ": Invalid artifact specified: [groupId: " + groupId + ", artifactId: " + artifactId + ", classifier: " + classifier + ", version: " + version + "]");
+                    	}
+                    } else {
+                        artifacts.add(new Artifact(groupId, artifactId, type, classifier, version));
+                    }
+					
+	                artifactId = "";
+	                classifier = "";
+	                type = JAR;
+				}
+			} else if (TABLE_CELL.equals(localName)) {
+				curCol++;
+				String value = contents.toString().trim();
+				contents.setLength(0);
+				
+				if (parsingColumnHeaders) {
+					value = value.toLowerCase();
+					if (value.startsWith("group")) {
+                        columnHeaders.put(curCol, ColumnType.GROUP_COLUMN);
+                    } else if (value.startsWith("artifact")) {
+                        columnHeaders.put(curCol, ColumnType.ARTIFACT_COLUMN);
+                    } else if (value.startsWith("type")) {
+                        columnHeaders.put(curCol, ColumnType.TYPE_COLUMN);
+                    } else if (value.startsWith("version")) {
+                        columnHeaders.put(curCol, ColumnType.VERSION_COLUMN);
+                    } else if (value.startsWith("classifier") || value.startsWith("alternate")) {
+                        columnHeaders.put(curCol, ColumnType.CLASSIFIER_COLUMN);
+                    }
+				} else {
+					ColumnType colType = columnHeaders.get(curCol);
+					if (colType != null) {
+						switch (colType) {
+							case GROUP_COLUMN:
+								if (!value.isEmpty()) {
+									groupId = value;
+								}
+								break;
+							case ARTIFACT_COLUMN:
+								if (!value.isEmpty()) {
+									artifactId = value;
+								}
+								break;
+							case TYPE_COLUMN:
+								if (!value.isEmpty()) {
+									type = value;
+								}
+								break;
+							case VERSION_COLUMN:
+								if (!value.isEmpty()) {
+									version = value;
+								}
+								break;
+							case CLASSIFIER_COLUMN:
+								if (!value.isEmpty()) {
+									classifier = value;
+								}
+								break;
+						}
+					}
+				}
+			}
+		}
+
+		@Override
+		public void endDocument() throws SAXException {
+			project.log(curRow + " rows read from " + odsFile, Project.MSG_VERBOSE);
+		}
+
+		@Override
+		public void characters(char[] ch, int start, int length) throws SAXException {
+			contents.append(ch, start, length);
+		}
+	}
 }
