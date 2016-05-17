c3f41c95bd7d2083a5dca1b53394f89937151049
Dave Brosius <dbrosius@mebigfatguy.com>
2016-05-16 21:33:53 -0400

    fix parsing of blank cells in xlsx

--- src/com/mebigfatguy/yank/XlsxParser.java
+++ src/com/mebigfatguy/yank/XlsxParser.java
@@ -22,178 +22,180 @@ import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.DefaultHandler;
 import org.xml.sax.helpers.XMLReaderFactory;
 
-public class XlsxParser  implements SpreadsheetParser {
+public class XlsxParser implements SpreadsheetParser {
 
-	private static final String SHARED_STRINGS = "xl/sharedStrings.xml";
-	private static final String CONTENT_STREAM = "xl/worksheets/sheet1.xml";
-	private static final String SHARED_INDEX = "si";
-	private static final String TABLE_ROW = "row";
-	private static final String TABLE_CELL = "c";
-	
-	private Project project;
-	private File xlsxFile;
-	private Map<Integer, String> sharedStrings = new HashMap<Integer, String>();
-	private List<Artifact> artifacts = new ArrayList<Artifact>();
-	
-	@Override
-	public List<Artifact> getArtifactList(Project proj, File spreadsheet) throws IOException {
-		
-		project = proj;
-		xlsxFile = spreadsheet;
-		
-		readSharedStrings();
-		
-		ZipInputStream zipIs = null;
-		InputStream contentStream = null;
-		try {
-			zipIs = new ZipInputStream(new BufferedInputStream(new FileInputStream(xlsxFile)));
-			contentStream = findContentStream(zipIs);
-			XMLReader r = XMLReaderFactory.createXMLReader();
-			r.setContentHandler(new XlsxHandler());
-			r.parse(new InputSource(contentStream));
-		} catch (SAXException e) {
-			throw new IOException("Failed creating xml reader for xlsx file (" + xlsxFile +")", e);
-		} finally {
-			Closer.close(contentStream);
-			Closer.close(zipIs);
-		}
-		
-		return artifacts;
-	}
+    private static final String SHARED_STRINGS = "xl/sharedStrings.xml";
+    private static final String CONTENT_STREAM = "xl/worksheets/sheet1.xml";
+    private static final String SHARED_INDEX = "si";
+    private static final String TABLE_ROW = "row";
+    private static final String TABLE_CELL = "c";
 
-	private void readSharedStrings() throws IOException {
-		ZipInputStream zipIs = null;
-		InputStream contentStream = null;
-		try {
-			zipIs = new ZipInputStream(new BufferedInputStream(new FileInputStream(xlsxFile)));
-			contentStream = findSharedStringsStream(zipIs);
-			XMLReader r = XMLReaderFactory.createXMLReader();
-			r.setContentHandler(new SharedStringHandler());
-			r.parse(new InputSource(contentStream));
-		} catch (SAXException e) {
-			throw new IOException("Failed creating xml reader for xlsx file (" + xlsxFile +")", e);
-		} finally {
-			Closer.close(contentStream);
-			Closer.close(zipIs);
-		}
-	}
-	
-	private InputStream findSharedStringsStream(ZipInputStream zipIs) throws IOException {
-		ZipEntry entry;
-		while ((entry = zipIs.getNextEntry()) != null) {
-			if (SHARED_STRINGS.equals(entry.getName())) {
-				long size = entry.getSize();
-				return new LengthLimitingInputStream(zipIs, (size < 0) ? Long.MAX_VALUE : size);
-			}
-		}
-		
-		throw new IOException("xlsx file has no shared strings stream (" + xlsxFile +")");
-	}
-	
-	private InputStream findContentStream(ZipInputStream zipIs) throws IOException {
-		ZipEntry entry;
-		while ((entry = zipIs.getNextEntry()) != null) {
-			if (CONTENT_STREAM.equals(entry.getName())) {
-				long size = entry.getSize();
-				return new LengthLimitingInputStream(zipIs, (size < 0) ? Long.MAX_VALUE : size);
-			}
-		}
-		
-		throw new IOException("ods file has no content stream (" + xlsxFile +")");
-	}
-	
-	class SharedStringHandler extends DefaultHandler {
+    private Project project;
+    private File xlsxFile;
+    private Map<Integer, String> sharedStrings = new HashMap<Integer, String>();
+    private List<Artifact> artifacts = new ArrayList<Artifact>();
 
-		private StringBuilder contents = new StringBuilder();
-		private int index = 0;
-		
-		@Override
-		public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-			if (SHARED_INDEX.equals(localName)) {
-				contents.setLength(0);
-			}
-		}
+    @Override
+    public List<Artifact> getArtifactList(Project proj, File spreadsheet) throws IOException {
 
-		@Override
-		public void endElement(String uri, String localName, String qName) throws SAXException {
-			if (SHARED_INDEX.equals(localName)) {
-				String value = contents.toString().trim();
-				sharedStrings.put(Integer.valueOf(index++), value);
-			}
-		}
+        project = proj;
+        xlsxFile = spreadsheet;
 
-		@Override
-		public void characters(char[] ch, int start, int length) throws SAXException {
-			contents.append(ch, start, length);
-		}
-	}
-	
-	class XlsxHandler extends DefaultHandler {
+        readSharedStrings();
 
-		private int curRow = 0;
-		private int curCol = 0;
-		private boolean parsingColumnHeaders = true;
-		private Map<Integer, ColumnType> columnHeaders = new HashMap<Integer, ColumnType>();
-		private StringBuilder contents = new StringBuilder();
-		private String groupId = "";
+        ZipInputStream zipIs = null;
+        InputStream contentStream = null;
+        try {
+            zipIs = new ZipInputStream(new BufferedInputStream(new FileInputStream(xlsxFile)));
+            contentStream = findContentStream(zipIs);
+            XMLReader r = XMLReaderFactory.createXMLReader();
+            r.setContentHandler(new XlsxHandler());
+            r.parse(new InputSource(contentStream));
+        } catch (SAXException e) {
+            throw new IOException("Failed creating xml reader for xlsx file (" + xlsxFile + ")", e);
+        } finally {
+            Closer.close(contentStream);
+            Closer.close(zipIs);
+        }
+
+        return artifacts;
+    }
+
+    private void readSharedStrings() throws IOException {
+        ZipInputStream zipIs = null;
+        InputStream contentStream = null;
+        try {
+            zipIs = new ZipInputStream(new BufferedInputStream(new FileInputStream(xlsxFile)));
+            contentStream = findSharedStringsStream(zipIs);
+            XMLReader r = XMLReaderFactory.createXMLReader();
+            r.setContentHandler(new SharedStringHandler());
+            r.parse(new InputSource(contentStream));
+        } catch (SAXException e) {
+            throw new IOException("Failed creating xml reader for xlsx file (" + xlsxFile + ")", e);
+        } finally {
+            Closer.close(contentStream);
+            Closer.close(zipIs);
+        }
+    }
+
+    private InputStream findSharedStringsStream(ZipInputStream zipIs) throws IOException {
+        ZipEntry entry;
+        while ((entry = zipIs.getNextEntry()) != null) {
+            if (SHARED_STRINGS.equals(entry.getName())) {
+                long size = entry.getSize();
+                return new LengthLimitingInputStream(zipIs, (size < 0) ? Long.MAX_VALUE : size);
+            }
+        }
+
+        throw new IOException("xlsx file has no shared strings stream (" + xlsxFile + ")");
+    }
+
+    private InputStream findContentStream(ZipInputStream zipIs) throws IOException {
+        ZipEntry entry;
+        while ((entry = zipIs.getNextEntry()) != null) {
+            if (CONTENT_STREAM.equals(entry.getName())) {
+                long size = entry.getSize();
+                return new LengthLimitingInputStream(zipIs, (size < 0) ? Long.MAX_VALUE : size);
+            }
+        }
+
+        throw new IOException("ods file has no content stream (" + xlsxFile + ")");
+    }
+
+    class SharedStringHandler extends DefaultHandler {
+
+        private StringBuilder contents = new StringBuilder();
+        private int index = 0;
+
+        @Override
+        public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+            if (SHARED_INDEX.equals(localName)) {
+                contents.setLength(0);
+            }
+        }
+
+        @Override
+        public void endElement(String uri, String localName, String qName) throws SAXException {
+            if (SHARED_INDEX.equals(localName)) {
+                String value = contents.toString().trim();
+                sharedStrings.put(Integer.valueOf(index++), value);
+            }
+        }
+
+        @Override
+        public void characters(char[] ch, int start, int length) throws SAXException {
+            contents.append(ch, start, length);
+        }
+    }
+
+    class XlsxHandler extends DefaultHandler {
+
+        private int curRow = 0;
+        private int curCol = 0;
+        private boolean parsingColumnHeaders = true;
+        private Map<Integer, ColumnType> columnHeaders = new HashMap<Integer, ColumnType>();
+        private StringBuilder contents = new StringBuilder();
+        private String groupId = "";
         private String artifactId = "";
         private String type = JAR;
         private String version = "";
         private String classifier = "";
-		
-		@Override
-		public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-			contents.setLength(0);
-			
-			if (TABLE_ROW.equals(localName)) {
-				curRow++;
-				curCol = 0;
 
-			} else if (TABLE_CELL.equals(localName)) {
-				contents.setLength(0);
-			}
-		}
+        @Override
+        public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+            contents.setLength(0);
 
-		@Override
-		public void endElement(String uri, String localName, String qName) throws SAXException {
-			if (TABLE_ROW.equals(localName)) {
-				if (parsingColumnHeaders) {
-					if (columnHeaders.size() > 0) {
-						if (columnHeaders.size() < 3)
-							throw new BuildException("Input yank xlsx file (" + xlsxFile + ") does not contains GroupId, ArtifactId, or Version columns");
-						parsingColumnHeaders = false;
-					}
-				} else {
-					if (groupId.isEmpty() || artifactId.isEmpty() || version.isEmpty()) {
-						if (!(groupId.isEmpty() && artifactId.isEmpty() && version.isEmpty())) {
-	                    	if (groupId.isEmpty() || version.isEmpty()) {
-	                    		project.log("Row " + curRow + ": Invalid artifact specified: [groupId: " + groupId + ", artifactId: " + artifactId + ", classifier: " + classifier + ", version: " + version + "]");
-	                    	}
-						}
+            if (TABLE_ROW.equals(localName)) {
+                curRow++;
+                curCol = 0;
+
+            } else if (TABLE_CELL.equals(localName)) {
+                String cellNum = attributes.getValue("r");
+                curCol = cellNum.charAt(0) - 'A';
+            }
+        }
+
+        @Override
+        public void endElement(String uri, String localName, String qName) throws SAXException {
+            if (TABLE_ROW.equals(localName)) {
+                if (parsingColumnHeaders) {
+                    if (columnHeaders.size() > 0) {
+                        if (columnHeaders.size() < 3) {
+                            throw new BuildException("Input yank xlsx file (" + xlsxFile + ") does not contains GroupId, ArtifactId, or Version columns");
+                        }
+                        parsingColumnHeaders = false;
+                    }
+                } else {
+                    if (groupId.isEmpty() || artifactId.isEmpty() || version.isEmpty()) {
+                        if (!(groupId.isEmpty() && artifactId.isEmpty() && version.isEmpty())) {
+                            if (groupId.isEmpty() || version.isEmpty()) {
+                                project.log("Row " + curRow + ": Invalid artifact specified: [groupId: " + groupId + ", artifactId: " + artifactId
+                                        + ", classifier: " + classifier + ", version: " + version + "]");
+                            }
+                        }
                     } else {
                         artifacts.add(new Artifact(groupId, artifactId, type, classifier, version));
                     }
-					
-	                artifactId = "";
-	                classifier = "";
-	                type = JAR;
-				}
-			} else if (TABLE_CELL.equals(localName)) {
-				curCol++;
-				String value = contents.toString().trim();
-				contents.setLength(0);
 
-				if (!value.isEmpty()) {
-					Integer index = Integer.valueOf(value);
-					value = sharedStrings.get(index);
-					if (value == null) {
-						throw new BuildException("Input yank xlsx file (" + xlsxFile + ") has missing shared string for index " + index + " at row " + curRow);
-					}
-				}
-				
-				if (parsingColumnHeaders) {
-					value = value.toLowerCase(Locale.ENGLISH);
-					if (value.startsWith("group")) {
+                    artifactId = "";
+                    classifier = "";
+                    type = JAR;
+                }
+            } else if (TABLE_CELL.equals(localName)) {
+                String value = contents.toString().trim();
+                contents.setLength(0);
+
+                if (!value.isEmpty()) {
+                    Integer index = Integer.valueOf(value);
+                    value = sharedStrings.get(index);
+                    if (value == null) {
+                        throw new BuildException("Input yank xlsx file (" + xlsxFile + ") has missing shared string for index " + index + " at row " + curRow);
+                    }
+                }
+
+                if (parsingColumnHeaders) {
+                    value = value.toLowerCase(Locale.ENGLISH);
+                    if (value.startsWith("group")) {
                         columnHeaders.put(curCol, ColumnType.GROUP_COLUMN);
                     } else if (value.startsWith("artifact")) {
                         columnHeaders.put(curCol, ColumnType.ARTIFACT_COLUMN);
@@ -204,49 +206,49 @@ public class XlsxParser  implements SpreadsheetParser {
                     } else if (value.startsWith("classifier") || value.startsWith("alternate")) {
                         columnHeaders.put(curCol, ColumnType.CLASSIFIER_COLUMN);
                     }
-				} else {
-					ColumnType colType = columnHeaders.get(curCol);
-					if (colType != null) {
-						switch (colType) {
-							case GROUP_COLUMN:
-								if (!value.isEmpty()) {
-									groupId = value;
-								}
-								break;
-							case ARTIFACT_COLUMN:
-								if (!value.isEmpty()) {
-									artifactId = value;
-								}
-								break;
-							case TYPE_COLUMN:
-								if (!value.isEmpty()) {
-									type = value;
-								}
-								break;
-							case VERSION_COLUMN:
-								if (!value.isEmpty()) {
-									version = value;
-								}
-								break;
-							case CLASSIFIER_COLUMN:
-								if (!value.isEmpty()) {
-									classifier = value;
-								}
-								break;
-						}
-					}
-				}
-			}
-		}
+                } else {
+                    ColumnType colType = columnHeaders.get(curCol);
+                    if (colType != null) {
+                        switch (colType) {
+                            case GROUP_COLUMN:
+                                if (!value.isEmpty()) {
+                                    groupId = value;
+                                }
+                            break;
+                            case ARTIFACT_COLUMN:
+                                if (!value.isEmpty()) {
+                                    artifactId = value;
+                                }
+                            break;
+                            case TYPE_COLUMN:
+                                if (!value.isEmpty()) {
+                                    type = value;
+                                }
+                            break;
+                            case VERSION_COLUMN:
+                                if (!value.isEmpty()) {
+                                    version = value;
+                                }
+                            break;
+                            case CLASSIFIER_COLUMN:
+                                if (!value.isEmpty()) {
+                                    classifier = value;
+                                }
+                            break;
+                        }
+                    }
+                }
+            }
+        }
 
-		@Override
-		public void endDocument() throws SAXException {
-			project.log(curRow + " rows read from " + xlsxFile, Project.MSG_VERBOSE);
-		}
+        @Override
+        public void endDocument() throws SAXException {
+            project.log(curRow + " rows read from " + xlsxFile, Project.MSG_VERBOSE);
+        }
 
-		@Override
-		public void characters(char[] ch, int start, int length) throws SAXException {
-			contents.append(ch, start, length);
-		}
-	}
+        @Override
+        public void characters(char[] ch, int start, int length) throws SAXException {
+            contents.append(ch, start, length);
+        }
+    }
 }
