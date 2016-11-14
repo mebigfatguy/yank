f0454c7c221d5c90037a44c4a1dfb273dfae1014
Dave Brosius <dbrosius@mebigfatguy.com>
2016-11-14 06:04:27 -0500

    update cp dates

--- src/com/mebigfatguy/yank/SpreadsheetParser.java
+++ src/com/mebigfatguy/yank/SpreadsheetParser.java
@@ -1,7 +1,7 @@
 /*
  * yank - a maven artifact fetcher ant task
- * Copyright 2013-2015 MeBigFatGuy.com
- * Copyright 2013-2015 Dave Brosius
+ * Copyright 2013-2016 MeBigFatGuy.com
+ * Copyright 2013-2016 Dave Brosius
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
