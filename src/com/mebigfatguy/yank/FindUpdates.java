f10839ffe21e1258eb472733844a01fa5f235b90
Dave Brosius <dbrosius@mebigfatguy.com>
2017-06-27 16:58:18 -0400

    protect against xxe attacks

--- src/com/mebigfatguy/yank/FindUpdates.java
+++ src/com/mebigfatguy/yank/FindUpdates.java
@@ -26,29 +26,34 @@ public class FindUpdates implements Runnable {
     private List<Artifact> artifacts;
     File findUpdatesFile;
     List<String> repositories;
-    
+
     public FindUpdates(Project proj, List<Artifact> afs, File updatesFile, List<String> servers) {
         project = proj;
         artifacts = afs;
         findUpdatesFile = updatesFile;
         repositories = servers;
     }
-    
+
     @Override
     public void run() {
         PrintWriter pw = null;
         try {
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            dbf.setExpandEntityReferences(false);
+            dbf.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+            dbf.setFeature("http://xml.org/sax/features/external-general-entities", false);
+            dbf.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+
             DocumentBuilder db = dbf.newDocumentBuilder();
             XPathFactory xpf = XPathFactory.newInstance();
             XPath xp = xpf.newXPath();
             XPathExpression xpe = xp.compile("/metadata/versioning/versions/version[last()]/text()");
-            
+
             pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(new FileOutputStream(findUpdatesFile), "UTF-8")));
             for (Artifact artifact : artifacts) {
                 for (String server : repositories) {
                     URL u = artifact.toMetaDataURL(server);
-                    
+
                     InputStream is = null;
                     try {
                         is = u.openStream();
@@ -57,30 +62,30 @@ public class FindUpdates implements Runnable {
                         }
                         is = new BufferedInputStream(is);
                         Document d = db.parse(is);
-                        
+
                         String latest = (String) xpe.evaluate(d, XPathConstants.STRING);
                         if (newer(artifact, latest)) {
                             pw.println(artifact + " has newer version: " + latest);
                         }
                         break;
-                        
+
                     } finally {
                         Closer.close(is);
                     }
                 }
-            }            
+            }
         } catch (Exception e) {
             project.log("Failed generating updated versions file " + findUpdatesFile, e, Project.MSG_ERR);
         } finally {
             Closer.close(pw);
         }
     }
-    
+
     private static boolean newer(Artifact artifact, String latest) {
         if ((latest == null) || latest.isEmpty()) {
             return false;
         }
-        
+
         return latest.compareTo(artifact.getVersion()) > 0;
     }
 
